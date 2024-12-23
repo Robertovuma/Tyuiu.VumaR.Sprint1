7a2ab46b4ddead2e7133a4b7c3af11ae4588845b
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-24 02:19:36 +0500

    Task4

---
+++ Tyuiu.VumaR.Sprint1.Task4.V3.Test/Test1.cs
@@ -0,0 +1,20 @@
+﻿
+using Tyuiu.VumaR.Sprint1.Task4.V3.Lib;
+
+namespace Tyuiu.VumaR.Sprint1.Task4.V3.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x, y;
+            x = 27;
+            y = 24;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
