15d30407575d7fb289b7ec3f037839f1563d4c4b
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-24 02:14:23 +0500

    Task3

---
+++ Tyuiu.VumaR.Sprint1.Task3.V16.Test/Test1.cs
@@ -0,0 +1,20 @@
+﻿
+using Tyuiu.VumaR.Sprint1.Task3.V16.Lib;
+
+
+namespace Tyuiu.VumaR.Sprint1.Task3.V16.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x1 = 1;
+            double x2 = 1;
+            var res = ds.CoeffOfQuadraticEquation(x1, x2);
+            Assert.AreEqual(-2, res);
+        }
+    }
+}
