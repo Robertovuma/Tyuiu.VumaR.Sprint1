e2de66818b7eeefdd895e96dcb7f04a43bf85d12
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-24 01:50:16 +0500

    Task1

---
+++ Tyuiu.VumaR.Sprint1.Task1.V15.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿
+using Tyuiu.VumaR.Sprint1.Task1.V15.Lib;
+
+namespace Tyuiu.VumaR.Sprint1.Task1.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 5.0;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(2, res);
+        }
+    }
+}
