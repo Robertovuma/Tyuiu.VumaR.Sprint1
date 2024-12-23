c533af8109a467b140f1956abf86c82152a75efe
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-24 02:52:13 +0500

    Task7

---
+++ Tyuiu.VumaR.Sprint1.Task7.V17.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+
+namespace Tyuiu.VumaR.Sprint1.Task7.V17.Lib
+{
+    public class DataService : ISprint1Task7V17
+    {
+        public double Calculate(double x, double y)
+        {
+            if (Math.Cos((12 * y) - 4) != 0)
+                return Math.Round((1 + Math.Sin(Math.Sqrt((x * x) + 1))) / (Math.Cos((12 * y) - 4)), 3);
+            else
+                return 0;
+        }
+    }
+}
---
+++ Tyuiu.VumaR.Sprint1.Task7.V17.Test/Test1.cs
@@ -0,0 +1,20 @@
+﻿
+using Tyuiu.VumaR.Sprint1.Task7.V17.Lib;
+
+namespace Tyuiu.VumaR.Sprint1.Task7.V17.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x, y;
+            x = 0;
+            y = 1;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(-12.656, res);
+        }
+    }
+}
